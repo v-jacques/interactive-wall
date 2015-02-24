25555ce2279c478e4aa1315208d91e3a2348de90
Jacques <viniciusjacques@gmail.com>
2015-02-23 23:24:41 -0500

    Leap Motion "Hello World" example

--- src/Main.java
+++ src/Main.java
@@ -5,22 +5,22 @@ public class Main {
 
 	public static void main(String[] args) {
 		// Create a sample listener and controller
-        SampleListener listener = new SampleListener();
-        Controller controller = new Controller();
+		SampleListener listener = new SampleListener();
+		Controller controller = new Controller();
 
-        // Have the sample listener receive events from the controller
-        controller.addListener(listener);
+		// Have the sample listener receive events from the controller
+		controller.addListener(listener);
 
-        // Keep this process running until Enter is pressed
-        System.out.println("Press Enter to quit...");
-        try {
-            System.in.read();
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+		// Keep this process running until Enter is pressed
+		System.out.println("Press Enter to quit...");
+		try {
+			System.in.read();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 
-        // Remove the sample listener when done
-        controller.removeListener(listener);
+		// Remove the sample listener when done
+		controller.removeListener(listener);
 	}
 
 }
