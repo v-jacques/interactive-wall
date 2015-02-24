20d54c7ed3fed83c4b43be94efa233b209e27325
Jacques <viniciusjacques@gmail.com>
2015-02-23 23:23:37 -0500

    Leap Motion "Hello World" example

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,26 @@
+import java.io.IOException;
+import com.leapmotion.leap.*;
 
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		// Create a sample listener and controller
+        SampleListener listener = new SampleListener();
+        Controller controller = new Controller();
 
+        // Have the sample listener receive events from the controller
+        controller.addListener(listener);
+
+        // Keep this process running until Enter is pressed
+        System.out.println("Press Enter to quit...");
+        try {
+            System.in.read();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+        // Remove the sample listener when done
+        controller.removeListener(listener);
 	}
 
 }
