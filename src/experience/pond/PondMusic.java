0bda728ca6052f0959fc937b223a79bd6c33af11
Jacques <viniciusjacques@gmail.com>
2015-04-27 00:23:59 -0400

    tried to add a water background sound

--- src/experience/pond/PondMusic.java
+++ src/experience/pond/PondMusic.java
@@ -19,6 +19,15 @@ public class PondMusic {
 	private static MediaPlayer backgroundMusicPlayer = new MediaPlayer(
 			backgroundMusic);
 
+	// private static File backgroundWater = new
+	// File("src/media/StreamNoise.mp3");
+	// private static final String WATER_BACKGROUND_MUSIC = backgroundWater
+	// .toURI().toString();
+	// private static Media backgroundWaterMusic = new Media(
+	// WATER_BACKGROUND_MUSIC);
+	// private static MediaPlayer backgroundWaterMusicPlayer = new MediaPlayer(
+	// backgroundWaterMusic);
+
 	// private static Timeline fadePlay = new Timeline(new
 	// KeyFrame(Duration.ZERO,
 	// ae -> backgroundMusicPlayer.play(), new KeyValue(
@@ -36,18 +45,52 @@ public class PondMusic {
 			new KeyFrame(new Duration(800), ae -> backgroundMusicPlayer.stop(),
 					new KeyValue(backgroundMusicPlayer.volumeProperty(), 0.0)));
 
+	// private static Timeline fadeWaterPause = new Timeline(new KeyFrame(
+	// Duration.ZERO, new KeyValue(
+	// backgroundWaterMusicPlayer.volumeProperty(), 0.25)),
+	// new KeyFrame(new Duration(800), ae -> backgroundWaterMusicPlayer
+	// .pause(), new KeyValue(backgroundWaterMusicPlayer
+	// .volumeProperty(), 0.0)));
+	// private static Timeline fadeWaterStop = new Timeline(new KeyFrame(
+	// Duration.ZERO, new KeyValue(
+	// backgroundWaterMusicPlayer.volumeProperty(), 0.25)),
+	// new KeyFrame(new Duration(800), ae -> backgroundWaterMusicPlayer
+	// .stop(), new KeyValue(backgroundWaterMusicPlayer
+	// .volumeProperty(), 0.0)));
+
 	public static void playBackgroundMusic() {
 		backgroundMusicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
 		backgroundMusicPlayer.setVolume(0.25);
 		backgroundMusicPlayer.setStartTime(Duration.seconds(5));
 		backgroundMusicPlayer.play();
+
+		// backgroundWaterMusicPlayer.setCycleCount(MediaPlayer.INDEFINITE);
+		// backgroundWaterMusicPlayer.setVolume(0.25);
+		// backgroundWaterMusicPlayer.setStartTime(Duration.seconds(5));
+		// backgroundWaterMusicPlayer.play();
+
+		// AudioStream backgroundMusic;
+		// AudioData musicData;
+		// AudioPlayer musicPlayer = AudioPlayer.player;
+		// ContinuousAudioDataStream loop = null;
+		// try {
+		// backgroundMusic = new AudioStream(new
+		// FileInputStream("src/media/StreamNoise.wav"));
+		// musicData = backgroundMusic.getData();
+		// loop = new ContinuousAudioDataStream(musicData);
+		// musicPlayer.start(loop);
+		// } catch (IOException error) {
+		// System.out.println(error);
+		// }
 	}
 
 	public static void pauseBackgroundMusic() {
 		fadePause.play();
+		// fadeWaterPause.play();
 	}
 
 	public static void stopBackgroundMusic() {
 		fadeStop.play();
+		// fadeWaterStop.play();
 	}
 }
